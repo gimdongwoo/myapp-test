16e8880b9f6487a785356ca17d028b892cbf80b7
Woo Gim <woo@chequer.io>
2019-05-27 16:06:24 +0900

    Updates

---
+++ precache-manifest.e402a6f2c41230d18f3f9fa2ffe1367d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f160e69d34b1565a9b89b9d8fbba0c",
+    "url": "/myapp-test/index.html"
+  },
+  {
+    "revision": "8041e46a152578d916ec",
+    "url": "/myapp-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/myapp-test/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "8041e46a152578d916ec",
+    "url": "/myapp-test/static/js/main.2795d7ba.chunk.js"
+  },
+  {
+    "revision": "631efa904d306eec1559",
+    "url": "/myapp-test/static/js/runtime~main.ce4d433f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
